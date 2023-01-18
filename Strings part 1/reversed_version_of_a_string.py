96d6c89f90200a00acd9d40001117c90baabec9a
Mahdi hasan Shuvo <mahdishuvo99@gmail.com>
2023-01-18 14:25:59 +0600

    Create reversed_version_of_a_string.py

---
+++ Strings part 1/reversed_version_of_a_string.py
@@ -0,0 +1,13 @@
+#S=[start:stop:step]
+#S=[::-1] -1 here means reverse
+S="Hello"
+print(S[::-1])
+k=""
+print(k[::-1])
+P="Wo"
+print(P[::-1])
+
+#Option 2
+L="Python"
+reversed_word=L[::-1]
+print(reversed_word)
