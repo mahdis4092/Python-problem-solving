89b2e90cf16137be0d39f6e75f96fe64afb0fe90
Mahdi hasan Shuvo <mahdishuvo99@gmail.com>
2023-01-22 21:37:52 +0600

    count_repeated_characters

---
+++ Strings part 2/count_repeated_characters.py
@@ -0,0 +1,14 @@
+s="corporation"
+repeated_count=0
+repeated_char=[]
+for char in s:
+    if(s.count(char)>1) and (char not in repeated_char):
+        repeated_count+=1
+        repeated_char.append(char)
+
+print(repeated_count)
+if len(repeated_char)>0:
+    for char in sorted(repeated_char):
+        print(char,end=" ")
+else:
+    print("none")
