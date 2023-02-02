6d372a659c9c77fb94adad34cc9b8b3d7ff38cdb
Mahdi hasan Shuvo <mahdishuvo99@gmail.com>
2023-02-02 13:57:38 +0600

    for loop while loop commit

---
+++ For and while loop 1/Multiplication_table.py
@@ -0,0 +1,5 @@
+given_num=5
+print("== Multiplication Of Table ==")
+for i in range(1,11):
+    mul=given_num*i
+    print(f"{given_num} X {i} ={mul} ")
---
+++ For and while loop 1/print_all_capital_alphabet.py
@@ -0,0 +1,7 @@
+for i in range(65,91):
+    x=chr(i)
+    print(x)
+
+#ord give number of character
+print(ord("B"))
+#output 66
